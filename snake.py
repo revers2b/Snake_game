d8c74020016d31ea82f4b4bb618af24e55cef2e9
Inaros <vkevinas99@gmail.com>
2023-02-27 23:00:12 +0200

    working game

--- snake.py
+++ snake.py
@@ -99,9 +99,11 @@ while True:
     else:
         snake_body.pop()
 
+    # Spawn
     if not food_spawn:
         food_pos = [random.randrange(1,(frame_size_x // square_size)) * square_size,
                 random.randrange(1,(frame_size_y // square_size)) * square_size]
+        food_spawn = True
         
     game_window.fill(black)
     for pos in snake_body:
@@ -110,7 +112,7 @@ while True:
             square_size -2, square_size))
         
     pygame.draw.rect(game_window, red, pygame.Rect(food_pos[0],
-                                        food_pos[1], square_size, square_size))
+                    food_pos[1], square_size, square_size))
     
 
     for block in snake_body[1:]:
